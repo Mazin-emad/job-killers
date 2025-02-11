222bd2239fc72ac1323a68c8545c234ebabfb2b7
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-02-11 18:25:14 +0200

    footer added

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import "primeicons/primeicons.css";
 import NavBar from "./components/NavBar.vue";
 import { RouterView } from "vue-router";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <NavBar />
   <RouterView />
+  <Footer />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer class="bg-green-700 py-4">
+    <div class="container mx-auto px-4 flex justify-between items-center">
+      <p class="text-white">
+        &copy; 2023 Job Killers. By
+        <a
+          href="https://github.com/Mazin-emad"
+          class="text-orange-500 hover:text-green-500"
+          target="_blank"
+          rel="noopener"
+          >Mazin Emad</a
+        >.
+      </p>
+      <div class="flex items-center justify-center">
+        <a
+          href="https://github.com/Mazin-emad"
+          class="text-white hover:text-green-500"
+          target="_blank"
+          rel="noopener"
+          ><i class="pi pi-github text-xl"></i
+        ></a>
+        <a
+          href="https://www.linkedin.com/in/mazin-emad10l10/"
+          class="text-white hover:text-green-500 ml-4"
+          target="_blank"
+          rel="noopener"
+          ><i class="pi pi-linkedin text-xl"></i
+        ></a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100026578280594"
+          class="text-white hover:text-green-500 ml-4"
+          target="_blank"
+          rel="noopener"
+          ><i class="pi pi-facebook text-xl"></i
+        ></a>
+        <a
+          href="https://twitter.com/MazinEmad685945"
+          class="text-white hover:text-green-500 ml-4"
+          target="_blank"
+          rel="noopener"
+          ><i class="pi pi-twitter text-xl"></i
+        ></a>
+      </div>
+    </div>
+  </footer>
+</template>
