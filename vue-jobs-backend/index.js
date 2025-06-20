2f974a82efe1e87eeea46ea9a8a5dc8cd8e4a7a2
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-06-20 19:24:31 +0300

    fix deployment the 13 time

--- vue-jobs-backend/index.js
+++ vue-jobs-backend/index.js
@@ -1,13 +1,14 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const cors = require("cors");
-const jobRoutes = require("./routes/jobRoutes.js");
+const jobRoutes = require("../routes/jobRoutes");
+require("dotenv").config();
+
+const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://job-killers.vercel.app/"],
+    origin: ["http://localhost:5173", "https://job-killers.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -17,13 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/jobs", jobRoutes);
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected!"));
-
 app.get("/", (req, res) => {
   res.send("Welcome to the Vue-Jobs API!, By Mazin Emad");
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("MongoDB connected");
 });
+
+// export for Vercel
+module.exports = app;
